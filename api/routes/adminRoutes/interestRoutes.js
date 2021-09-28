5d2ac5a0ef4d321f7321bb814d869c0f97ac7fd0
keshav suman <keshavsuman96@gmail.com>
2021-09-28 23:45:44 +0530

    asfghr

--- api/routes/adminRoutes/interestRoutes.js
+++ api/routes/adminRoutes/interestRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/createInterest', auth, interestValidator.createInterest, interest.createInterest);
 router.delete('/deleteInterest', auth, interestValidator.deleteInterest, interest.deleteInterest);
 router.post('/getInterests', interest.getInterests);
-router.post('/updateInterest', auth, interestValidator.updateInterest, interest.editInterest);
+router.post('/updateInterest', auth, interest.editInterest);
 router.get('/updateInterestStatus',auth, interest.updateInterestStatus);
 
 module.exports = router;
