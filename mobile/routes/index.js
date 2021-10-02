eaac8d537853b268119685748ed69521d5bc05f8
keshav suman <keshavsuman96@gmail.com>
2021-10-02 15:37:02 +0530

    asrg

--- mobile/routes/index.js
+++ mobile/routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const admin = require('./admin');
 const user = require('./user');
 
-router.get('/api/mobile/v1/admin', function (req, res, next) {
+router.get('/api/app/v1/admin', function (req, res, next) {
   res.send('Welcome to younity Admin Apis');
 });
-router.get('/api/mobile/v1/user', function (req, res, next) {
+router.get('/api/app/v1/user', function (req, res, next) {
   res.send('Welcome to younity User Apis');
 });
-router.use('/api/mobile/v1/admin', admin);
-router.use('/api/mobile/v1/user', user);
+router.use('/api/app/v1/admin', admin);
+router.use('/api/app/v1/user', user);
 
 
 module.exports = router;
