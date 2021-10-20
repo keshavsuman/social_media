b7aa9c4262c28f1fa2cf83094bfd8392e7b07e19
keshav suman <keshavsuman96@gmail.com>
2021-10-20 19:31:24 +0530

    sagf

--- mobile/models/bookmarks.js
+++ mobile/models/bookmarks.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookmarkSchema = mongoose.Schema({
-    posts:[{type:mongoose.Schema.Types.ObjectId,ref:'posts',required:true}],
+    posts:[{type:mongoose.Schema.Types.ObjectId,ref:'post',required:true}],
     user:{type:mongoose.Schema.Types.ObjectId,ref:'user',required:true}
 },{timestamps:true});
 
