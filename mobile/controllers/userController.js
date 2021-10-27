95dfd64cd61243f463afbcd147c274a883c45754
keshav suman <keshavsuman96@gmail.com>
2021-10-27 12:18:47 +0530

    fahg

--- mobile/controllers/userController.js
+++ mobile/controllers/userController.js
@@ -818,7 +818,7 @@ module.exports.cancelRequest = async (req,res)=>{
         await connections.findOneAndUpdate({user:req.data._id},{
             $pullAll:{requested:[mongoose.Types.ObjectId(req.body.id)]}
         });
-        await User.findById(req.data._id,{
+        await User.findByIdAndUpdate(req.data._id,{
             $pullAll:{sentRequests:[mongoose.Types.ObjectId(req.body.id)]}
         });
         responseManagement.sendResponse(res,httpStatus.OK,'Request canceled',{});
