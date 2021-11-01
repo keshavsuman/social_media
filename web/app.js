1daf53c5c9819c2cc61ce02eb3386bfd25ad3984
keshav suman <keshavsuman96@gmail.com>
2021-11-01 11:51:02 +0530

    dsfg

--- web/app.js
+++ web/app.js
@@ -45,7 +45,7 @@ app.use(helmet());
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+app.use('/api/web/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 app.use(mongoSanitize({
   onSanitize: ({ req, key }) => {
     console.warn(`This request[${key}] is sanitized`, req);
