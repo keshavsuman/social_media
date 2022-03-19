8cdf9f2b2d345329f870f9966bcbf6a7fcf3b4bc
keshav suman <keshavsuman96@gmail.com>
2022-03-19 15:12:46 +0530

    Initial commit

--- web/controllers/userController.js
+++ web/controllers/userController.js
@@ -33,6 +33,8 @@ module.exports.login = async (req, res) => {
                     const user_data = {
                         _id: user._id,
                         email: user.email,
+                        first_name:user.first_name,
+                        last_name:user.last_name,
                         mobile: user.mobile,
                         profile_setup: user.profile_setup
                     };
